35d4ac444c2c553c50e522bb1b229a8bf72bdc0f
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-19 10:49:21 -0300

    Corrigindo erros de digitacao e codigo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 //Criando uma rota
-app.get('/dados', function(){
+app.get('/dados', function(req, res){
 	res.send({
 		nome: "Fernando Mazola",
 		idade: 20,
@@ -12,6 +12,6 @@ app.get('/dados', function(){
 });
 
 
-app.listen((){
+app.listen(3000, function(){
 	console.log("Api rodando na porta 3000");
-})
+});
