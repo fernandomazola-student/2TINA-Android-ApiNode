fb942e4796a0d3692e4961cadc30337e84c767b2
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-19 10:44:43 -0300

    Implementado no corpo da rota os dados e adicionado listen

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ var app = express();
 
 //Criando uma rota
 app.get('/dados', function(){
-	
+	res.send({
+		nome: "Fernando Mazola",
+		idade: 20,
+		sexo: "Masculino"
+	});
 });
+
+
+app.listen((){
+	console.log("Api rodando na porta 3000");
+})
